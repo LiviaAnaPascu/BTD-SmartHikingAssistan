f5606ead6d81351e80d5ba5eeca890bdd31a8d6f
11902039 <e11902039@student.tuwien.ac.at>
2023-06-27 16:07:04 +0200

    Various Fixes, Notifications etc

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ SCREEN_UV = 2
 SCREEN_WEATHER = 3
 
 # Current screen variable
+lcd.clear()
 current_screen = SCREEN_MAIN
 axp.setLcdBrightness(100)
 
@@ -31,7 +32,7 @@ axp.setLcdBrightness(100)
 thStepP = 0.2  # save only peaks above this threshold
 thLiftP = 0.35
 thLiftN = -0.2  # save only peaks below this threshold
-thLiftP2 = 0.3
+thLiftP2 = 0.32
 thFallP = 1.8  # check for robustness
 
 # Different variables for signal processing
@@ -68,6 +69,19 @@ uvInd = 0
 temperature = 0
 tempFeels = 0
 weather = ""
+
+# FallAlerts
+emergency = False
+emName = ""
+PWM0 = machine.PWM(2, freq=500, duty=50, timer=0)
+PWM0.pause()
+
+# Notifications
+drinkTime = 360000
+# drinkTime = 1000 #Testing
+sunTime = 0
+PWM1 = machine.PWM(2, freq=300, duty=50, timer=0)  # Notify alert
+PWM1.pause()
 
 # Communication
 sessionId = "HOBBIT_HIKE"
@@ -183,11 +197,14 @@ def buttonA_wasPressed():
     global fallen
     global inlift
     global screenset
+    global emergency
 
     if fallen:
         fallen = False
     elif inlift:
         inlift = False
+    elif emergency:
+        emergency = False
     else:
         inlift = True
 
@@ -213,12 +230,16 @@ def main_screen():
     Steps = M5Rect(7, 140, 120, 25, 0x707070, 0x707070)
     Weather = M5Rect(7, 175, 120, 25, 0x707070, 0x707070)
     rectangle0 = M5Rect(7, 210, 120, 25, 0x707070, 0x707070)
-    StepImage = M5Img(10, 143, "res/footprint.png", True)
-    TempImage = M5Img(10, 178, "res/weather.png", True)
-    UVImage = M5Img(10, 213, "res/beach.png", True)
-    StepCounter = M5TextBox(34, 144, str(steps), lcd.FONT_DejaVu18, 0x000000, rotate=0)
-    Temperature = M5TextBox(34, 180, '{}°C'.format(temperature), lcd.FONT_DejaVu18, 0x000000, rotate=0)
-    label1 = M5TextBox(35, 215, uv, lcd.FONT_DejaVu18, 0x000000, rotate=0)
+    # StepImage = M5Img(10, 143, "res/footprint.png", True)
+    # TempImage = M5Img(10, 178, "res/weather.png", True)
+    # UVImage = M5Img(10, 213, "res/beach.png", True)
+    # StepCounter = M5TextBox(34, 144, str(steps), lcd.FONT_DejaVu18, 0x000000, rotate=0)
+    # Temperature = M5TextBox(34, 180, '{}°C'.format(temperature), lcd.FONT_DejaVu18, 0x000000, rotate=0)
+    # label1 = M5TextBox(35, 215, uv, lcd.FONT_DejaVu18, 0x000000, rotate=0)
+
+    StepCounter = M5TextBox(15, 145, str(steps) + " steps", lcd.FONT_Default, 0x000000, rotate=0)
+    Temperature = M5TextBox(15, 181, '{}°C'.format(temperature), lcd.FONT_Default, 0x000000, rotate=0)
+    label1 = M5TextBox(15, 216, "UV " + uv, lcd.FONT_Default, 0x000000, rotate=0)
 
 
 def steps_screen():
@@ -227,55 +248,68 @@ def steps_screen():
     setScreenColor(0x000000)
     axp.setLcdBrightness(100)
     Frame = M5Rect(7, 60, 120, 120, 0x707070, 0x707070)
-    StepImage = M5Img(60, 70, "res/footprint.png", True)
+    # StepImage = M5Img(60, 70, "res/footprint.png", True)
     label0 = M5TextBox(25, 100, "Today's count:", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
-    StepCounter = M5TextBox(43, 120, str(steps), lcd.FONT_DejaVu18, 0x000000, rotate=0)
+    StepCounter = M5TextBox(43, 120, str(steps) + " steps", lcd.FONT_Default, 0x000000, rotate=0)
 
 
 def uv_screen():
-    lcd.clear()
     global uv
     global uvInd
+    lcd.clear()
     axp.setLcdBrightness(100)
     setScreenColor(0x000000)
     Frame = M5Rect(7, 60, 120, 120, 0x707070, 0x707070)
-    UVImage = M5Img(60, 70, "res/beach.png", True)
-    label0 = M5TextBox(47, 100, "UV index:", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
-    UVIndex = M5TextBox(35, 120, str(uvInd) + " " + uv, lcd.FONT_DejaVu18, 0x000000, rotate=0)
-    # TODO: Adapt this text based on uv level
-    label2 = M5TextBox(12, 145, "Protection needed!", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
+    # UVImage = M5Img(60, 70, "res/beach.png", True)
+    label0 = M5TextBox(43, 100, "UV index:", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
+    UVIndex = M5TextBox(10, 120, str(uvInd) + " " + uv, lcd.FONT_Default, 0x000000, rotate=0)
+    text = ""
+    if (uv == "Low"):
+        text = "No Protection needed"
+    elif (uv == "Moderate"):
+        text = "Protect slightly"
+    elif (uv == "High"):
+        text = "Protection needed"
+    elif (uv == "Very High"):
+        text = "Stay in Shadows!"
+
+    text = "No Protection needed"
+    label2 = M5TextBox(15, 145, text, lcd.FONT_DefaultSmall, 0x000000, rotate=0)
 
 
 def weather_screen():
-    lcd.clear()
     global tempFeels
     global temperature
     global weather
+    lcd.clear()
     axp.setLcdBrightness(100)
     setScreenColor(0x000000)
     Frame = M5Rect(7, 60, 120, 120, 0x707070, 0x707070)
     # TODO: Adapt picture based on weather
-    WeatherImage = M5Img(60, 70, "res/weather.png", True)
-    label0 = M5TextBox(30, 100, "Temperature:", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
-    Temperature = M5TextBox(45, 120, str(temperature) + " \u00B0C", lcd.FONT_DejaVu18, 0x000000, rotate=0)
-    label2 = M5TextBox(20, 145, "Feels like " + str(tempFeels) + " \u00B0C", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
+    # WeatherImage = M5Img(60, 70, "res/weather.png", True)
+    label1 = M5TextBox(15, 75, "Weather: " + weather, lcd.FONT_DefaultSmall, 0x000000, rotate=0)
+    label0 = M5TextBox(30, 105, "Temperature:", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
+    Temperature = M5TextBox(35, 125, str(temperature) + " \u00B0C", lcd.FONT_Default, 0x000000, rotate=0)
+    label2 = M5TextBox(15, 155, "Feels like " + str(tempFeels) + " \u00B0C", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
 
 
 def fall_screen():
+    global PWM0
+
     lcd.clear()
     axp.setLcdBrightness(100)
     setScreenColor(0x000000)
     Frame = M5Rect(5, 60, 124, 160, 0x707070, 0x707070)
-    StepImage = M5Img(60, 70, "res/warning.png", True)
+    # StepImage = M5Img(60, 70, "res/warning.png", True)
     label0 = M5TextBox(20, 100, "Emergency Alert!", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
     label2 = M5TextBox(23, 130, "Did you Fall?", lcd.FONT_Default, 0x000000, rotate=0)
     label3 = M5TextBox(10, 149, "Press Button A", lcd.FONT_Default, 0x000000, rotate=0)
     label4 = M5TextBox(10, 168, "to cancel Alert", lcd.FONT_Default, 0x000000, rotate=0)
 
     for i in range(0, 50):
-        PWM0 = machine.PWM(2, freq=500, duty=50, timer=0)
+        PWM0.resume()
         wait_ms(90)
-        PWM0 = machine.PWM(2, freq=200, duty=0, timer=0)
+        PWM0.pause()
         M5Led.on()
         wait_ms(90)
         M5Led.off()
@@ -283,23 +317,31 @@ def fall_screen():
 
     send_emergency()
     wait_ms(10)
-    label2.setText("An Alert was")
-    label3.setText("sent to all other")
-    label4.setText("linked Devices!")
+
+    lcd.clear()
+    axp.setLcdBrightness(100)
+    setScreenColor(0x000000)
+    Frame = M5Rect(5, 60, 124, 160, 0x707070, 0x707070)
+    label0 = M5TextBox(20, 100, "Emergency Alert!", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
+    label2 = M5TextBox(23, 130, "An Alert was", lcd.FONT_Default, 0x000000, rotate=0)
+    label3 = M5TextBox(10, 149, "sent to all other", lcd.FONT_Default, 0x000000, rotate=0)
+    label4 = M5TextBox(10, 168, "linked Devices!", lcd.FONT_Default, 0x000000, rotate=0)
 
 
-def alert_screen(name):
+def alert_screen():
+    global emName
+    global PWM0
     lcd.clear()
     axp.setLcdBrightness(100)
     setScreenColor(0xffffff)
     Frame = M5Rect(7, 60, 120, 120, 0xffffff, 0xffffff)
-    StepImage = M5Img(60, 70, "res/warning.png", True)
+    # StepImage = M5Img(60, 70, "res/warning.png", True)
     label0 = M5TextBox(20, 114, "Emergency Alert!", lcd.FONT_DefaultSmall, 0x000000, rotate=0)
     label2 = M5TextBox(49, 130, "From", lcd.FONT_Default, 0x000000, rotate=0)
-    label3 = M5TextBox(33, 149, "name", lcd.FONT_Default, 0x000000, rotate=0)
-    PWM0 = machine.PWM(2, freq=500, duty=50, timer=0)
+    label3 = M5TextBox(33, 149, "Device " + emName, lcd.FONT_Default, 0x000000, rotate=0)
+    PWM0.resume()
     wait_ms(90)
-    PWM0 = machine.PWM(2, freq=200, duty=0, timer=0)
+    PWM0.pause()
     M5Led.on()
     wait_ms(90)
     M5Led.off()
@@ -307,18 +349,40 @@ def alert_screen(name):
 
 
 def notification(text):
-    wait(1)
+    global screenset
+    global PWM1
+
+    wait_ms(10)
     axp.setLcdBrightness(100)
-    Frame = M5Rect(7, 30, 120, 50, 0x707070, 0xedef6e)
-    label0 = M5TextBox(20, 40, text, lcd.FONT_Default, 0x000000, rotate=0)
-    wait(2)
+    Frame = M5Rect(7, 30, 120, 80, 0x707070, 0xedef6e)
+    if text == "drink":
+        label0 = M5TextBox(10, 55, "Don't forget", lcd.FONT_Default, 0x000000, rotate=0)
+        label1 = M5TextBox(10, 75, "to hydrate!", lcd.FONT_Default, 0x000000, rotate=0)
+    elif text == "sun":
+        label0 = M5TextBox(10, 55, "Reapply", lcd.FONT_Default, 0x000000, rotate=0)
+        label1 = M5TextBox(10, 75, "sunscreen!", lcd.FONT_Default, 0x000000, rotate=0)
+
+    for i in range(0, 10):
+        PWM1.resume()
+        wait_ms(100)
+        PWM0.pause()
+        M5Led.on()
+        wait_ms(100)
+        M5Led.off()
+        wait_ms(2)
+
     Frame.hide()
     label0.hide()
+    label1.hide()
+    screenset = False
 
 
 # Communication Functions
 
 def handle_emergency_messages():
+    global emergency
+    global emName
+
     host = wlan.ifconfig()[0]
     port = 420
 
@@ -329,13 +393,17 @@ def handle_emergency_messages():
 
     wait_ms(100)
 
+    wait_ms(100)
+    M5Led.off()
     client_socket, addr = server_socket.accept()
     # print(f"Accepted connection from {addr[0]}:{addr[1]}")
 
     res = str(client_socket.recv(1024).decode())
     name = res.split('*')[1]
     # Trigger buzzer here and show UI stuff
-    alert_screen(name)
+    # alert_screen(name)
+    emergency = True
+    emName = name
 
     # close connection
     client_socket.close()
@@ -388,10 +456,13 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect('HH72VM_43BD_2.4G', 'dtF9dK6G')
 wlan.connect('HannibalsHorrificHotspot', 'honeybunny')
-wait_ms(100)
 
 # We need the public IP otherwise we can't get any location data
+label1 = M5TextBox(30, 140, "Set Up", lcd.FONT_DefaultSmall, 0xffffff, rotate=0)
+label0 = M5TextBox(20, 160, "Find Ip Address", lcd.FONT_DefaultSmall, 0xffffff, rotate=0)
 publicIP = urequests.get(url='https://api.ipify.org').text
+
+label0.setText("Server Connection")
 connect_to_server()
 register_device()
 responseWeather = get_weather(publicIP)
@@ -404,13 +475,26 @@ weather = response[4]
 close_connection()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+label1.setText("")
+label0.setText("")
+if (uv == "Low"):
+    sunTime = 0
+elif (uv == "Moderate"):
+    sunTime = 1440000  # every 5 hours
+elif (uv == "High"):
+    sunTime = 720000  # every 2 hours
+elif (uv == "Very High"):
+    sunTime = 360000  # every hour
+
+# testing
+# sunTime = 1500
+
 # Set Up Threading
 _thread.start_new_thread(handle_emergency_messages, ())
 
 # Setup for screen
 setScreenColor(0x000000)
-# axp.setLcdBrightness(0)
-last_activity_time = time.time()
+axp.setLcdBrightness(0)
 battery = (str((map_value((axp.getBatVoltage()), 3.7, 4.1, 0, 100))) + str('%'))
 date = rtc.now()
 # Extract the time components
@@ -476,6 +560,7 @@ while True:
         c1 = 0
         window1 = []
         time1 = []
+        screenset = False
         # M5Led.off()
 
     if (c2 == samplenr):  # Process and reset Window 2
@@ -484,7 +569,24 @@ while True:
         c2 = 0
         window2 = []
         time2 = []
+        screenset = False
         # M5Led.off()
+
+    if (not screenset):
+        battery = (str((map_value((axp.getBatVoltage()), 3.7, 4.1, 0, 100))) + str('%'))
+        date = rtc.now()
+        # Extract the time components
+        hour = int(date[3])
+        minute = int(date[4])
+        # Format the time string as "HH:MM"
+        time_string = "{:02d}:{:02d}".format(hour, minute)
+        # Extract the date components
+        year = int(date[0])
+        month = int(date[1])
+        day = int(date[2])
+        # Format the date string as "Mon dd"
+        month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        date_string = "{} {:02d}".format(month_names[month - 1], day)
 
     # do all the checking for thresholds
     if (curFil < 0.07) and (curFil > -0.07):  # Neutral range, save it
@@ -541,6 +643,12 @@ while True:
                 time2.append(curTime)
                 lastSaved = 0
 
+    # Notification Time
+    if (totalC % drinkTime) == 0:
+        notification("drink")
+    if (totalC % sunTime) == 0:
+        notification("sun")
+
     # Set Up for next Round
     prevLpf = curLpf
     prevFil = curFil
@@ -551,16 +659,25 @@ while True:
     if (fallen and not screenset):
         # first check for 10 secs then send alarm message
         # possibly exchange
-        # fall_screen()
-        send_emergency()
+        fall_screen()
         screenset = True
-        fallen = False
     elif (inlift and not screenset):
-        # main_screen()
+        if current_screen == SCREEN_MAIN:
+            main_screen()
+        elif current_screen == SCREEN_STEPS:
+            steps_screen()
+        elif current_screen == SCREEN_UV:
+            uv_screen()
+        elif current_screen == SCREEN_WEATHER:
+            weather_screen()
         screenset = True
-    elif (not screenset):
-        lcd.clear()
-        setScreenColor(0x000000)
-        # axp.setLcdBrightness(0)
+    elif (emergency):
+        alert_screen()
+    else:
+        if (not screenset):
+            lcd.clear()
+            setScreenColor(0x000000)
+            axp.setLcdBrightness(0)
+            screenset = True
 
     wait_ms(wait)
